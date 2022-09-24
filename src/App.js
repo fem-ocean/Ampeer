b871b6dd98159e5a368ae5c14c8918f146db3ecf
Femi <abiolaoshin@gmail.com>
2022-09-24 07:52:32 +0100

    imported react in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./Components/Header";
+import React from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from "./Components/Home";
 import PropertyInfo from "./Components/PropertyInfo";
